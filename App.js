2e83c76417505bd2c14a0ce6b92c113ff037e8da
tanyas02200 <tanyas022000@gmail.com>
2023-02-08 12:40:02 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import {
+  View,
+   ActivityIndicator,
+   ProgressViewIOS,
+    Button,
+    Alert,
+    Dimensions,
+    Text,
+    Platform,
+    ProgressBarAndroid,
+    StyleSheet
+  }     from "react-native";
+const {height,width} =Dimensions.get("window");
+export default function App(){
+  const onButtonPress = () => {
+    Alert.alert(`${new Date().toLocaleTimeString()} button press`);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    <View style ={styles.page2}>
+    
+        <Button title="click me" onPress={onButtonPress} />
+        <ActivityIndicator size="large" color ="blue" />
     </View>
+    <View style ={styles.page}>
+   {Platform.OS === "ios" && <ProgressViewIOS progress ={0.1} />} 
+   {Platform.OS === "android" && 
+   <ProgressBarAndroid 
+   styleAttr="Horizontal"
+   indeterminate ={false}
+   color = "blue"
+   progress={0.5} />}
+        <Text style ={[styles.text,styles.selectedText]}>height:{height}</Text>
+        <Text style ={styles.text}>width:{width}</Text> 
+        <Text style ={styles.text}>OS: {Platform.OS}</Text>
+    </View>
+    </>
+   
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  page :{
+    flex:1,
+    flexDirection: "row",
+    // justifyContent:"center",
+    marginTop: 50,
+    backgroundColor:"#DDD"
   },
+  page2: {
+    marginTop:50
+  },
+  text:{
+    fontSize:20,
+    color:"red",
+    backgroundColor:"yellow",
+    margin:10,
+    padding:5
+  },
+  selectedText:{
+    backgroundColor:"red",
+    color:"yellow"
+  }
 });
+
